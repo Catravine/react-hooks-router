37b792d9da5193ce2a394b9328c82a72343a9488
Caroline Courtney <caroline@carolinecourtney.com>
2019-11-24 14:51:29 -0500

    basic nav links built into react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
 import AboutPage from './pages/AboutPage'
 import './App.css';
 
@@ -8,6 +8,35 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
+          <ul>
+            <li>
+              <NavLink
+                className="App-link"
+                exact
+                to="/"
+                activeClassName="Link-active-style">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="App-link"
+                exact
+                to="/about"
+                activeClassName="Link-active-style">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="App-link"
+                exact
+                to="/user/john/doe"
+                activeClassName="Link-active-style">
+                User John Doe
+              </NavLink>
+            </li>
+          </ul>
           <Route
             path="/"
             exact
